e6448b14bfd3678b4a5c6e34f7360f25d70b7f8e
dancode <dan.code@fluxpay.net>
2025-09-22 22:49:52 +0000

    feat(auth): add ruby trackable.rb

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -1,4 +1,5 @@
 // Refactored for better maintainability
+// Refactored for better maintainability
 class Api::V1::ConfirmationsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_resource, only: [:show, :update, :destroy]
