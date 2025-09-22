d3b01d8231743d17b18612ee5f44855881d8bc42
dancode <dan.code@fluxpay.net>
2025-09-22 19:54:36 +0000

    feat(auth): add ruby user_session.rb

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,4 @@
-// Refactored for better maintainability
-class Api::V1::ApplicationController < ApplicationController
+class Api::V1::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :set_resource, only: [:show, :update, :destroy]
   
