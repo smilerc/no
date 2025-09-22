29c339bacaadc8669e99e65a3ffeb3a776807ce3
dancode <dan.code@fluxpay.net>
2025-09-22 23:23:52 +0000

    Refactor auth_controller.rb for better maintainability

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -1,9 +1,4 @@
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-class Api::V1::AuthController < ApplicationController
+class Api::V1::PaymentsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_resource, only: [:show, :update, :destroy]
   
