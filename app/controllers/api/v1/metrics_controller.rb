b2a70b8b830303df8ecd09251c3466ea0ed45bad
dancode <dan.code@fluxpay.net>
2025-09-23 17:07:02 +0000

    Refactor analytics_controller.rb for better maintainability

--- app/controllers/api/v1/metrics_controller.rb
+++ app/controllers/api/v1/metrics_controller.rb
@@ -1,5 +1,4 @@
-// Refactored for better maintainability
-class Api::V1::AnalyticsController < ApplicationController
+class Api::V1::MetricsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_resource, only: [:show, :update, :destroy]
   
