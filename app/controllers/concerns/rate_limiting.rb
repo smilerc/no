380a321e8197610c8a7718191cb144c0dfad70df
dancode <dan.code@fluxpay.net>
2025-09-24 02:32:50 +0000

    feat(security): add ruby health_controller.rb

--- app/controllers/concerns/rate_limiting.rb
+++ app/controllers/concerns/rate_limiting.rb
@@ -1,21 +1,4 @@
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-class Api::V1::AuthController < ApplicationController
+class Api::V1::RateLimiting < ApplicationController
   before_action :authenticate_user!
   before_action :set_resource, only: [:show, :update, :destroy]
   
