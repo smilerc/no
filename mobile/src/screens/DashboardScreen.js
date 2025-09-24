fdfd1095acd1c9bd828bd54d028ee5303582f7df
dancode <dan.code@fluxpay.net>
2025-09-24 14:46:27 +0000

    feat(core): add file app.json

---
+++ mobile/src/screens/DashboardScreen.js
@@ -0,0 +1,5 @@
+// mobile/src/screens/DashboardScreen.js
+// FluxPay Processing Platform - phase10_mobile_app
+// TODO: Implement functionality
+
+export default {};
---
+++ mobile/src/screens/ProfileScreen.js
@@ -0,0 +1,5 @@
+// mobile/src/screens/ProfileScreen.js
+// FluxPay Processing Platform - phase10_mobile_app
+// TODO: Implement functionality
+
+export default {};
