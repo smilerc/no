6f7b3cbcbfa6b5b79bcd704d53d04a3e044953e9
dancode <dan.code@fluxpay.net>
2025-09-24 15:49:51 +0000

    feat(core): add module WalletCard.js

---
+++ mobile/src/components/WalletCard.js
@@ -0,0 +1,5 @@
+// mobile/src/components/WalletCard.js
+// FluxPay Processing Platform - phase10_mobile_app
+// TODO: Implement functionality
+
+export default {};
---
+++ mobile/src/context/ThemeContext.js
@@ -0,0 +1,5 @@
+// mobile/src/context/ThemeContext.js
+// FluxPay Processing Platform - phase10_mobile_app
+// TODO: Implement functionality
+
+export default {};
