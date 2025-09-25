959ed107518020cefd7b93f382bb69cf267279f2
dancode <dan.code@fluxpay.net>
2025-09-25 11:58:01 +0000

    feat(core): add file FluxPayClient.php

---
+++ sdk/go/client_test.go
@@ -0,0 +1,5 @@
+// sdk/go/client_test.go
+// FluxPay Processing Platform - phase17_sdk
+// TODO: Implement functionality
+
+export default {};
---
+++ sdk/go/go.mod
@@ -0,0 +1,5 @@
+// sdk/go/go.mod
+// FluxPay Processing Platform - phase17_sdk
+// TODO: Implement functionality
+
+export default {};
