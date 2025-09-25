105bebf6a3d2fad38145043d24367929a5cd119f
dancode <dan.code@fluxpay.net>
2025-09-25 03:39:55 +0000

    feat(core): add config system_alerts.yml

--- app/controllers/monitoring/metrics_controller.rb
+++ app/controllers/monitoring/metrics_controller.rb
@@ -1,8 +1,4 @@
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-// Refactored for better maintainability
-class Api::V1::PaymentsController < ApplicationController
+class Api::V1::MetricsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_resource, only: [:show, :update, :destroy]
   
