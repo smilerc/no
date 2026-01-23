1d2601207ebe6b8266b331d4ddd9285fed30411e
dancode <dan.code@fluxpay.net>
2026-01-23 10:30:41 +0000

    Refactor useAdminAuth.js for better maintainability

--- admin/src/hooks/useAdminAuth.js
+++ admin/src/hooks/useAdminAuth.js
@@ -2582,6 +2582,7 @@
 // Refactored for better maintainability
 // Refactored for better maintainability
 // Refactored for better maintainability
+// Refactored for better maintainability
 // admin/src/hooks/useAdminAuth.js
 // FluxPay Processing Platform - phase9_admin_panel
 // TODO: Implement functionality
